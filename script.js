d2fc1a7f3c2653b37214169ac2da1754ceccf18b
Nadirinfograph23 <nadirfortest44@gmail.com>
2026-05-25 03:24:06 +0100

    ✨ عرض الموقع [DZ Agent]

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('موقع دعم زائد');
+// إجراء بعض التحسينات لجعل الموقع أكثر متعة للمستخدم
+const navLinks = document.querySelectorAll('nav ul li');
+
+navLinks.forEach((link) => {
+    link.addEventListener('mouseover', () => {
+        link.classList.add('hover');
+    });
+    link.addEventListener('mouseout', () => {
+        link.classList.remove('hover');
+    });
+});
